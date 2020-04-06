2a600477c0cd4b7310d8980638b7fb45fa5e382a
Henrique Ramos <ricci.henrique@outlook.com>
2020-04-06 20:22:14 -0300

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -4,8 +4,7 @@ class KMeans(object):
     def __init__(self, dados, k=2):
         self.k = k
         self.dados = dados
-        self.trata_dados()
-        
+        self.trata_dados() # Trata os dados caso haja campos que não sejam numéricos
         
     def train(self):
         centroids = []
@@ -15,21 +14,20 @@ class KMeans(object):
             for j in range(len(self.dados[0])):
                 centroids[i].append(self.dados[i][j])
         
-        while True:
-            # O K + 1 é para adicionar na ultima coluna quem possui a menor distancia1
+        while True:            
             self.distancias = []
 
-            for linha in range(len(self.dados)): # para cada linha dos dados
-                self.distancias.append([0] * (self.k + 1))
-                for centroids_index in range(self.k): # e para cada um dos centroids
+            for linha in range(len(self.dados)): # Para cada linha dos dados
+                self.distancias.append([0] * (self.k + 1)) # K + 1 serve para adicionar uma coluna a mais para guardar o index da menor distância
+                for centroids_index in range(self.k): # Para cada um dos centroids
                     distancia = 0
-                    for coluna in range(len(self.dados[0])): # calcula-se a distancia para cada atributo
+                    for coluna in range(len(self.dados[0])): # calcula-se a distancia de cada atributo
                         distancia += pow(centroids[centroids_index][coluna] - self.dados[linha][coluna], 2)
 
                     distancia = math.sqrt(distancia)
                     self.distancias[linha][centroids_index] = distancia
 
-                #calcular o menor
+                #calcula o menor e armazena o index na ultima coluna
                 self.distancias[linha][self.k] = self.distancias[linha].index(min(self.distancias[linha][ : self.k]))
             
             #calcula novo centroids            
@@ -44,15 +42,13 @@ class KMeans(object):
                 
                 count_centroids[self.distancias[i][self.k]] += 1
             
-            
-            for i in range(self.k):
+            for i in range(self.k): 
                 for j in range(len(self.dados[0])):
                     novos_centroids[i][j] /= count_centroids[i]
             
-            if(novos_centroids == centroids):
+            if(novos_centroids == centroids): # Verifica se não há mais variações nos centorides
                 break
-            centroids = novos_centroids
-            
+            centroids = novos_centroids            
                     
     def labels(self):
         l = []
