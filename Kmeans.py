c0152ba48663c63ee5ff66d9b9a53a50404d61cb
Henrique Ramos <ricci.henrique@outlook.com>
2020-04-06 20:01:33 -0300

    Add files via upload

---
+++ Kmeans.py
@@ -0,0 +1,75 @@
+class KMeans(object):
+    import math
+    
+    def __init__(self, dados, k=2):
+        self.k = k
+        self.dados = dados
+        self.trata_dados()
+        
+        
+    def train(self):
+        centroids = []
+        
+        for i in range(self.k): #inicializa os centroids com os primeiros dados
+            centroids.append([])            
+            for j in range(len(self.dados[0])):
+                centroids[i].append(self.dados[i][j])
+        
+        while True:
+            # O K + 1 é para adicionar na ultima coluna quem possui a menor distancia1
+            self.distancias = []
+
+            for linha in range(len(self.dados)): # para cada linha dos dados
+                self.distancias.append([0] * (self.k + 1))
+                for centroids_index in range(self.k): # e para cada um dos centroids
+                    distancia = 0
+                    for coluna in range(len(self.dados[0])): # calcula-se a distancia para cada atributo
+                        distancia += pow(centroids[centroids_index][coluna] - self.dados[linha][coluna], 2)
+
+                    distancia = math.sqrt(distancia)
+                    self.distancias[linha][centroids_index] = distancia
+
+                #calcular o menor
+                self.distancias[linha][self.k] = self.distancias[linha].index(min(self.distancias[linha][ : self.k]))
+            
+            #calcula novo centroids            
+            novos_centroids = [[]] * self.k
+            count_centroids = [0] * self.k            
+
+            for i in range(len(self.dados)):
+                if(novos_centroids[self.distancias[i][self.k]] == []):
+                    novos_centroids[self.distancias[i][self.k]] = [0] * len(self.dados[0])        
+                for j in range(len(self.dados[0])):
+                    novos_centroids[self.distancias[i][self.k]][j] += self.dados[i][j]                
+                
+                count_centroids[self.distancias[i][self.k]] += 1
+            
+            
+            for i in range(self.k):
+                for j in range(len(self.dados[0])):
+                    novos_centroids[i][j] /= count_centroids[i]
+            
+            if(novos_centroids == centroids):
+                break
+            centroids = novos_centroids
+            
+                    
+    def labels(self):
+        l = []
+        for i in self.distancias:
+            l.append(i[self.k])
+        
+        return l
+    
+    def trata_dados(self):
+        for i in range(len(self.dados[0])):
+            if(not (type(self.dados[0][i]) == int or type(self.dados[0][i]) == float)):
+                c = 0
+                obj = {}
+                for j in range(len(self.dados)):
+                    if(obj.get(self.dados[j][i]) == None):
+                        obj[self.dados[j][i]] = c
+                        c += 1
+                        
+                    self.dados[j][i] = obj[self.dados[j][i]]
+        
---
+++ README.md
@@ -0,0 +1,7 @@
+# K-Means
+<<<<<<< HEAD
+# KMeans-Implementacao
+=======
+
+Algoritmo de clusterização de dados feito em Python
+>>>>>>> 87d5bb18698e305105600a8d3c074da89a994da0
